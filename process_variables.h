6f35a10d66e73b35af9c99890c8e9aeaf75550a3
picklepeet <nikoavradopoulos@gmail.com>
2025-03-21 17:41:56 -0400

    clean header & add vscode/win configs for portability

--- process_variables.h
+++ process_variables.h
@@ -1,16 +1,12 @@
+#ifndef PROCESS_H
+#define PROCESS_H
 
-#ifndef process_details
-#define process_details
-
-typedef struct Process { /* A structure defined with typedef data type and named Process */
-   int PID;
-   int Arrival_Time;
-   int Burst_Time;
-   int Priority;
- } Process;
-
- /* Comparison functions to use for sorting with qsort */
-
- int sortPriority(const void *a, const void *b);
+typedef struct Process
+{
+  int pid;
+  int arrival_time;
+  int burst_time;
+  int priority;
+} Process;
 
 #endif
